a40d1db568fc9480939895d8e48299e0801d3c29
Luz-Davila <luz.davila.mayorga@est.una.ac.cr>
2026-05-24 22:41:37 -0600

    Fix import in Router component

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -10,7 +10,7 @@ import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
 import Admin from "../Pages/Admin/Admin";
 import Inventario from "../Pages/Inventario/Inventario";
-/*import Averias from "../Pages/Averias/Averias";*/
+/*import Averias from "../Pages/Averias/AveriasPage";*/
 
 
 const rootRoute = createRootRoute({
@@ -56,7 +56,7 @@ const inventarioRoute = createRoute({
     path: "/inventario",
     component: Inventario,
 });
-const routeTree = rootRoute.addChildren([homeRoute, abonadosRoute, adminRoute, /*averiasRoute,*/ inventarioRoute]);
+const routeTree = rootRoute.addChildren([homeRoute, abonadosRoute, adminRoute,/* averiasRoute,*/ inventarioRoute]);
 
 export const router = createRouter({
     routeTree,
